26bd5c9bbb6e9ff73853aef1ef438d2de7db791f
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-22 17:09:29 +0300

    Виконує завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const ulEl = document.querySelector('.gallery');
+
+const elements = galleryItems
+  .map(item => {
+      const strHTML = 
+      `<div class="gallery__item">
+       <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}" />
+       </a>
+      </div>`;
+    return strHTML;
+  })
+  .join("");
+ulEl.insertAdjacentHTML("afterbegin", elements);
+
+
+const onClick = (event) => {
+    event.preventDefault();
+    if (event.target.className === "gallery__image") {
+        const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt",  captionDelay: 250, })
+    }
+};
+
+ulEl.addEventListener('click', onClick);
+
 console.log(galleryItems);
